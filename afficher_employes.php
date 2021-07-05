5cfc14ffa5ae6c71ef32ba3b27345ab94bbd5e31
Tiago OC <tiagopokego2@gmail.com>
2021-07-05 16:35:09 +0200

    suppresion + debut recherche

--- afficher_employes.php
+++ afficher_employes.php
@@ -8,7 +8,29 @@ $fetchUsers = $connexion->query('SELECT * FROM employes');
 $users = $fetchUsers->fetchAll(PDO::FETCH_ASSOC);
 $recup = $connexion->query('SELECT * FROM employes');
 echo "<h3>Affichage de " . $recup->rowCount() . " Employes</h3>";
+
+if(isset($_GET['action']) && $_GET['action'] == 'suppression'){
+    $connexion->query("DELETE FROM employes WHERE id_employes = '$_GET[id_employes]' ");
+    header('location: afficher_employes.php');
+}
+if($_POST){
+
+    $rechercheTerm = $_POST['term'];
+
+    var_dump($rechercheTerm);
+
+    // raffrechir automatiquement aprés un post
+    echo "<meta http-equiv='refresh' content='0'>";
+}
 ?>
+<form method="post">
+    <div class="form-row">
+        <div class="col-sm-3">
+            <input class="form-control" type="text" placeholder="tapez un nom" name="term" />
+        </div>
+        <button type="submit" class="btn btn-primary">recherche</button>
+    </div>
+</form><br>
 <table class="table table-striped">
     <thead class="thead-dark">
         <tr>
@@ -18,7 +40,7 @@ echo "<h3>Affichage de " . $recup->rowCount() . " Employes</h3>";
                 echo "<th scope='col'>". $colonne['name'] ."</th>";
             }
             ?>
-            <th scope="col">Actions</th>
+            <th scope="col">supprimer</th>
         </tr>
     </thead>
     <tbody>
@@ -28,11 +50,11 @@ echo "<h3>Affichage de " . $recup->rowCount() . " Employes</h3>";
         <tr>     
             <?php
                 echo "<th scope='col'>" . $value['id_employes'] ."</th>";
-                for($i=0; $i < count($value); $i++){
+                for($i=1; $i < count($value); $i++){
                     echo "<td>" . $value[array_keys($value)[$i]] . "</td>";
                 }
                 echo "<td>";
-                        echo "<a href=\"?action=suppression&id_employes=$value[id_employes]\">Supprimer</a>";
+                        echo "<a class='btn btn-danger' href=\"?action=suppression&id_employes=$value[id_employes]\">Supprimer</a>";
                 echo "</td>";
             ?>
         </tr>
